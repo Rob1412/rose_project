747ee65aa6434aea8e87f1a0509a94150514e9d2
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2023-11-30 20:29:41 +0100

    color detection integration started

---
+++ app/api/fast.py
@@ -0,0 +1,50 @@
+# $WIPE_BEGIN
+
+from app.color_detection.color_detection import *
+
+# $WIPE_END
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allowing all middleware is optional, but good practice for dev purposes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# $WIPE_BEGIN
+# 💡 Preload the model to accelerate the predictions
+# We want to avoid loading the heavy Deep Learning model from MLflow at each `get("/predict")`
+# The trick is to load the model in memory when the Uvicorn server starts
+# and then store the model in an `app.state.model` global variable, accessible across all routes!
+# This will prove very useful for the Demo Day
+app.state.model = load_model()
+# $WIPE_END
+
+# http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
+@app.get("/predict")
+def predict(
+        file_path: str
+    ):      # 1
+    """
+    Make a single course prediction.
+    Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
+    Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
+    """
+
+    return find_pink(file_path)
+
+
+
+
+@app.get("/")
+def root():
+    # $CHA_BEGIN
+    return dict(greeting="Hello")
+    # $CHA_END
---
+++ app/color_detection/color_detection.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+
+def get_limits(color):
+    """
+    get_limits takes a color in BGR values and return upper and lower hue values.
+    example: lowerHue, upperHue = get_limits([220, 140, 255])
+    """
+
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    #Get the hue value
+    hue = hsvC[0][0][0]
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+
+
+
+def find_pink(image_path):
+    """
+    find_pink takes the path to image as input and return True if pink was detected
+    example: find_pink("raw_data/flowers/rose/9185768268_1e48d4d119_c.jpg")
+    """
+    #set default return value
+    found_pink = False
+
+    #Define pink in BGR color space
+    pink = [220, 140, 255]
+
+    #Get limits of pink
+    lowerHue, upperHue = get_limits(pink)
+
+    #Load the image
+    img = cv2.imread(image_path)
+
+    #Convert image from BGR into HSV color space
+    hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #Define mask
+    mask = cv2.inRange(hsvImage, lowerHue, upperHue)
+
+    #Identify how much pink is in the image
+    unique, counts = np.unique(mask, return_counts=True)
+
+    #threshold for amount of pink needed in the image
+    pink_threshold = 0.005
+
+    #check if pink was detected
+    if counts.shape[0] == 2:
+        if counts[1] / counts[0] > pink_threshold:
+            found_pink = True
+
+    return found_pink
