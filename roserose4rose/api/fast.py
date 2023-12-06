371affb0b2381e50f5c99b8efbd8a722f13762a4
Michael Smirnow <michael.smirnow@posteo.de>
2023-12-06 16:07:19 +0100

    fastAPI works for image uploads POST

--- roserose4rose/api/fast.py
+++ roserose4rose/api/fast.py
@@ -1,11 +1,13 @@
 # $WIPE_BEGIN
 
-from app.color_detection.color_detection import *
-
 # $WIPE_END
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import cv2
+from roserose4rose.color_detection.color_detection import find_pink, find_pink_imported_img
 
 app = FastAPI()
 
@@ -18,15 +20,6 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-# $WIPE_BEGIN
-# 💡 Preload the model to accelerate the predictions
-# We want to avoid loading the heavy Deep Learning model from MLflow at each `get("/predict")`
-# The trick is to load the model in memory when the Uvicorn server starts
-# and then store the model in an `app.state.model` global variable, accessible across all routes!
-# This will prove very useful for the Demo Day
-app.state.model = load_model()
-# $WIPE_END
-
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 @app.get("/predict")
 def predict(
@@ -38,10 +31,24 @@ def predict(
     Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
     """
 
-    return find_pink(file_path)
+    return find_pink("./raw_data/flowers/rose/"+file_path)
 
 
+@app.post('/upload_image')
+async def receive_image(img: UploadFile=File(...)):
+    ### Receiving and decoding the image
+    contents = await img.read()
 
+    nparr = np.fromstring(contents, np.uint8)
+    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+
+    ### Do cool stuff with your image.... For example face detection
+    response=find_pink_imported_img(cv2_img)
+
+    ### Encoding and responding with the image
+    #im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
+    #Response(content=im.tobytes(), media_type="image/png")
+    return response
 
 @app.get("/")
 def root():
--- roserose4rose/color_detection/color_detection.py
+++ roserose4rose/color_detection/color_detection.py
@@ -65,3 +65,39 @@ def find_pink(image_path):
             found_pink = True
 
     return found_pink
+
+
+def find_pink_imported_img(img):
+    """
+    find_pink takes the path to image as input and return True if pink was detected
+    example: find_pink("raw_data/flowers/rose/9185768268_1e48d4d119_c.jpg")
+    """
+    #set default return value
+    found_pink = False
+
+    #Define pink in BGR color space
+    pink = [220, 140, 255]
+
+    #Get limits of pink
+    lowerHue, upperHue = get_limits(pink)
+
+    #Load the image
+
+    #Convert image from BGR into HSV color space
+    hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #Define mask
+    mask = cv2.inRange(hsvImage, lowerHue, upperHue)
+
+    #Identify how much pink is in the image
+    unique, counts = np.unique(mask, return_counts=True)
+
+    #threshold for amount of pink needed in the image
+    pink_threshold = 0.005
+
+    #check if pink was detected
+    if counts.shape[0] == 2:
+        if counts[1] / counts[0] > pink_threshold:
+            found_pink = True
+
+    return found_pink
