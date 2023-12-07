503b68d2289bf5dabaa6e3823ae88f2682a8030f
Michael Smirnow <michael.smirnow@posteo.de>
2023-12-07 15:14:42 +0100

    classification and colour identification works with fastapi local

--- roserose4rose/api/fast.py
+++ roserose4rose/api/fast.py
@@ -7,8 +7,8 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 import cv2
-from roserose4rose.color_detection.color_detection import find_pink, find_pink_imported_img
-
+from roserose4rose.color_detection.color_detection import find_pink_imported_img
+from roserose4rose.flower_classificator.flower_classifier import predict
 app = FastAPI()
 
 # Allowing all middleware is optional, but good practice for dev purposes
@@ -21,34 +21,32 @@ app.add_middleware(
 )
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
-@app.get("/predict")
-def predict(
-        file_path: str
-    ):      # 1
-    """
-    Make a single course prediction.
-    Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
-    Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
-    """
-
-    return find_pink("./raw_data/flowers/rose/"+file_path)
 
 
 @app.post('/upload_image')
 async def receive_image(img: UploadFile=File(...)):
     ### Receiving and decoding the image
     contents = await img.read()
+    class_response=predict(contents)
+    pred_class=class_response[0]
+    pred_prob=round(class_response[1]*100)
+    returnval=f'This is a {pred_class} with probability {pred_prob}%'
+    if pred_class=="rose":
+        nparr = np.fromstring(contents, np.uint8)
+        cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+        colour_response=find_pink_imported_img(cv2_img)
 
-    nparr = np.fromstring(contents, np.uint8)
-    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+        if not colour_response:
+            returnval=f'This is a rose with probability {pred_prob}% but not in colour pink'
+        else:
+            returnval=f'This is a rose with probability {pred_prob}% and it is pink'
 
-    ### Do cool stuff with your image.... For example face detection
-    response=find_pink_imported_img(cv2_img)
+
 
     ### Encoding and responding with the image
     #im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
     #Response(content=im.tobytes(), media_type="image/png")
-    return response
+    return returnval
 
 @app.get("/")
 def root():
