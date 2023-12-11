6884e30243fca9300ac30c5aa68da3f6e23184cf
Michael Smirnow <michael.smirnow@posteo.de>
2023-12-11 17:57:33 +0100

    weired

--- roserose4rose/color_detection/color_detection.py
+++ roserose4rose/color_detection/color_detection.py
@@ -66,3 +66,53 @@ def find_pink_imported_img(img):
             pink_value = 1
 
     return pink_value
+
+def multi_color_detect_img(img):
+    """
+    find_pink takes an image as input and returns "0" for no pink found,
+    "1" for some pink found, and "2" for enough pink found
+    example: find_pink(image)
+    """
+    #set default return value
+    pink_value = 0
+    colorthresholds={}
+    colors={
+        'green':[42,239,60],
+        'yellow':[15,255,254],
+        'orange':[18,146,249],
+        'red':[22,13,246],
+        'lila':[239,13,246],
+        'purple':[237,5,145],
+        'blue':[237,9,5],
+        'aquamarin':[237,127,5],
+        'turquoise':[237,198,5],
+        'mint':[162,237,5],
+        'white':[245,245,245],
+        'black':[10,10,10]
+    }
+    #Define pink in BGR color space
+    #pink = [220, 140, 255]
+    for c in colors.keys():
+
+    #Get limits of c
+        lowerHue, upperHue = get_limits(colors[c])
+
+    #Convert image from BGR into HSV color space
+        hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    #Define mask
+        mask = cv2.inRange(hsvImage, lowerHue, upperHue)
+
+    #Identify how much pink is in the image
+        unique, counts = np.unique(mask, return_counts=True)
+
+    #threshold for amount of pink needed in the image
+        if counts.shape[0] == 2:
+            colorthresholds[c]=counts[1] / counts[0]
+    #check if pink was detected
+
+    return dict(sorted(colorthresholds.items(), key=lambda item: item[1])) #-item=biggest first
+
+
+image_path='/home/michael/code/Rob1412/rose_project/raw_data/flowers/magnolia/10599462_97cb6f005b_c.jpg'
+print(multi_color_detect_img(cv2.imread(image_path)))
